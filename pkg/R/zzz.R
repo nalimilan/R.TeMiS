0e67704d19a1005648fe8d4e83fee80b2af0f1ae
Milan Bouchet-Valat <nalimilan@club.fr>
2012-07-02 13:58:30 +0000

    Add workaround for JGR locale bug on Mac OS X
    JGR always sets locale to "C", which breaks Rcmdr UI and text handling.
    
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/r-temis@50 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -6,6 +6,15 @@
                 Rcmdr$plugins <- c(plugins, pkgname)
                 options(Rcmdr=Rcmdr)
                 closeCommander(ask=FALSE, ask.save=TRUE)
+
+                # Work around a bug in JGR: restore the correct locale
+                # https://www.rforge.net/bugzilla/show_bug.cgi?id=244
+                if (Sys.info()["sysname"] == "Darwin" && Sys.getlocale() == "C")  {
+                        loc <- system("defaults read -g AppleLocale", intern=TRUE)
+                        Sys.setlocale("LC_ALL", loc)
+                        rm(loc)
+                }
+
                 Commander()
         }
 }
