d5383503fb7208faf719a314106cec360bc0effd
Milan Bouchet-Valat <nalimilan@club.fr>
2012-07-02 13:59:40 +0000

    Factiva: Use shorter article IDs
    The original ID contains too many useless characters. We can safely
    remove the redundant 0, and almost as safely replace the long serial
    number with the last two chars. A collision can still happen if two
    articles from the same origin and the same day have the same last
    two chars, but the general scheme seems to be that these last two chars
    change first, and the beginning of the serial number identifies the larger
    series, which means it would not happen.
    
    A better solution would be to attribute unique letters from
    FactivaSource once we know all the IDs, but this is not possible with
    XMLSource currently.
    
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/r-temis@103 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tm.plugin.factiva/R/readFactiva.R
+++ pkg/tm.plugin.factiva/R/readFactiva.R
@@ -12,9 +12,13 @@ readFactiva <- tm::readXML(
                          strptime(sapply(XML::getNodeSet(node, "/article/publicationDate/date"), xmlValue),
                                   format="%Y-%m-%d")),
     Heading = list("node", "/article/headline"),
-    ID = list("function", function(node)
-              gsub("^distdoc:archive/ArchiveDoc::Article/", "",
-                   sapply(XML::getNodeSet(node, "/article/reference"), xmlValue))),
+    ID = list("function", function(node) {
+              str <- gsub("^distdoc:archive/ArchiveDoc::Article/", "",
+                           sapply(XML::getNodeSet(node, "/article/reference"), xmlValue))
+              # Extract useful information: origin, date, and two last characters to avoid collisions
+              m <- regmatches(str, regexec("^([A-Za-z]+)0*[1-9][0-9]([0-9][0-9][01][1-9][01][0-9]).*([A-Za-z0-9]{2})$", str))[[1]]
+              paste(m[2], "-", m[3], "-", m[4], sep="")
+    }),
     Origin = list("node", "/article/sourceName"),
     Language = list("function", function(node)
                     tolower(sapply(XML::getNodeSet(node, "/article/baseLanguage"), xmlValue))),
