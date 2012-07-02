bc5352f8e2c1e8556770aceaa7afd39390b7fa7a
Milan Bouchet-Valat <nalimilan@club.fr>
2012-07-02 13:59:09 +0000

    Factiva: Adapt to upcoming changes in tm and fix bugs with paragraphs
    tm 0.5-7.2 uses XML internal nodes, which fixes a few problems with
    the parsed XML, but we need to adapt a little to this by using xmlChildren().
    Also fix a bug with concatenating tailParagraphs: add a new line to avoid
    words from the end and the beginning of the next paragraph from being
    concatenated.
    
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/r-temis@78 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tm.plugin.factiva/R/FactivaSource.R
+++ pkg/tm.plugin.factiva/R/FactivaSource.R
@@ -1,6 +1,7 @@
 FactivaSource <- function(x, encoding = "UTF-8") {
     tm::XMLSource(x, function(tree) {
-        XML::xmlChildren(XML::xmlRoot(tree)$children$ppsArticleResponse[[1]])
+        sapply(XML::xmlChildren(XML::xmlChildren(XML::xmlChildren(XML::xmlRoot(tree))
+            $ppsArticleResponse)$ppsarticleResultSet), XML::xmlChildren)
     },
                   readFactiva, encoding)
 }
--- pkg/tm.plugin.factiva/R/readFactiva.R
+++ pkg/tm.plugin.factiva/R/readFactiva.R
@@ -2,34 +2,34 @@ readFactiva <- tm::readXML(
     spec = list(Author = list("function", function(node)
                 toupper(gsub("^\\s+|\\s+$", "",
                              gsub("\n|\\s+", " ",
-                                  sapply(XML::getNodeSet(node, "/ppsarticle/article/byline"), xmlValue))))),
+                                  sapply(XML::getNodeSet(node, "/article/byline"), xmlValue))))),
     Content = list("function", function(node)
-                   paste(sapply(XML::getNodeSet(node, "/ppsarticle/article/headline"), xmlValue),
-                         sapply(XML::getNodeSet(node, "/ppsarticle/article/leadParagraph"), xmlValue),
-                         sapply(XML::getNodeSet(node, "/ppsarticle/article/tailParagraphs"), xmlValue),
+                   paste(sapply(XML::getNodeSet(node, "/article/headline"), xmlValue),
+                         sapply(XML::getNodeSet(node, "/article/leadParagraph"), xmlValue),
+                         paste(sapply(XML::getNodeSet(node, "/article/tailParagraphs/paragraph"), xmlValue), collapse="\n"),
                          sep="\n\n")),
     DateTimeStamp = list("function", function(node)
-                         strptime(sapply(XML::getNodeSet(node, "/ppsarticle/article/publicationDate/date"), xmlValue),
+                         strptime(sapply(XML::getNodeSet(node, "/article/publicationDate/date"), xmlValue),
                                   format="%Y-%m-%d")),
-    Heading = list("node", "/ppsarticle/article/headline"),
+    Heading = list("node", "/article/headline"),
     ID = list("function", function(node)
               gsub("^distdoc:archive/ArchiveDoc::Article/", "",
-                   sapply(XML::getNodeSet(node, "/ppsarticle/article/reference"), xmlValue))),
-    Origin = list("node", "/ppsarticle/article/sourceName"),
+                   sapply(XML::getNodeSet(node, "/article/reference"), xmlValue))),
+    Origin = list("node", "/article/sourceName"),
     Language = list("function", function(node)
-                    tolower(sapply(XML::getNodeSet(node, "/ppsarticle/article/baseLanguage"), xmlValue))),
-    Edition = list("node", "/ppsarticle/article/edition"),
-    Section = list("node", "/ppsarticle/article/sectionName"),
-    Subject = list("node", "/ppsarticle/article/newsSubject/name"),
-    Coverage = list("node", "/ppsarticle/article/region/name"),
+                    tolower(sapply(XML::getNodeSet(node, "/article/baseLanguage"), xmlValue))),
+    Edition = list("node", "/article/edition"),
+    Section = list("node", "/article/sectionName"),
+    Subject = list("node", "/article/newsSubject/name"),
+    Coverage = list("node", "/article/region/name"),
     WordCount = list("function", function(node)
-                     as.numeric(sapply(XML::getNodeSet(node, "/ppsarticle/article/wordCount"), xmlValue))),
+                     as.numeric(sapply(XML::getNodeSet(node, "/article/wordCount"), xmlValue))),
     Pages = list("function", function(node)
-                 as.numeric(sapply(XML::getNodeSet(node, "/ppsarticle/article/pages"), xmlValue))),
-    Publisher = list("node", "/ppsarticle/article/publisherName"),
+                 as.numeric(sapply(XML::getNodeSet(node, "/article/pages"), xmlValue))),
+    Publisher = list("node", "/article/publisherName"),
     Rights = list("function", function(node)
                   gsub("^\\s+|\\s+$", "",
                        gsub("\n|\\s+", " ",
-                            sapply(XML::getNodeSet(node, "/ppsarticle/article/copyright"), xmlValue))))),
+                            sapply(XML::getNodeSet(node, "/article/copyright"), xmlValue))))),
     doc = PlainTextDocument())
 
