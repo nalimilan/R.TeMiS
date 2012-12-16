67e9a6d4ebd8e7d808a7b8b0fb24e4f97d148b4f
Milan Bouchet-Valat <nalimilan@club.fr>
2012-12-16 17:18:13 +0000

    Avoid opening Commander window twice on load
    After discussing this with John Fox, a solution was found, which
    required a few changes in Rcmdr itself so that we can run it
    without adding it to Depends, only to Imports. Because Rcmdr
    redefines some functions from tcltk, a warning would appear on
    install: to remove it, only import non-conflicting symbols we use
    from tcltk. This requires Rcmdr >= 1.9-3 (not yet on CRAN).
    
    Also fix indentation in zzz.R, and rename it to startup.R.
    
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/r-temis@369 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/rcmdr.temis/R/startup.R
@@ -0,0 +1,52 @@
+.onAttach <- function(libname, pkgname){
+  # Work around a bug with Java on Mac OS blocking Snowball and RWeka
+  if(Sys.info()["sysname"] == "Darwin")  {
+      Sys.setenv(NOAWT="true")
+  }
+
+  # Work around a bug in JGR: restore the correct locale
+  # https://www.rforge.net/bugzilla/show_bug.cgi?id=244
+  if(Sys.info()["sysname"] == "Darwin" && Sys.getlocale() == "C")  {
+      loc <- system("defaults read -g AppleLocale", intern=TRUE)
+      Sys.setlocale("LC_ALL", loc)
+  }
+
+  if (!interactive()) return()
+
+
+  Rcmdr <- options()$Rcmdr
+  plugins <- Rcmdr$plugins
+
+  if (!pkgname %in% plugins) {
+      Rcmdr$plugins <- c(plugins, pkgname)
+      Rcmdr$ask.on.exit <- FALSE
+      options(Rcmdr=Rcmdr)
+
+      if("package:Rcmdr" %in% search()) {
+          if(!getRcmdr("autoRestart")) {
+              closeCommander(ask=FALSE, ask.save=TRUE)
+              Commander()
+          }
+      }
+      else {
+          Commander()
+      }
+  }
+
+  # HTML.matrix() does not allow passing scientific separately,
+  # and vocabulary summary tables often end up printed in scientific notation
+  doItAndPrint(.gettext("# Prefer fixed to scientific notation"))
+  doItAndPrint('options(scipen=5)')
+
+  doItAndPrint("")
+  doItAndPrint(.gettext("# Print numbers with two significant digits"))
+  doItAndPrint('options(digits=2)')
+
+  # Keep in sync with disableBlackAndWhite()
+  doItAndPrint("")
+  doItAndPrint(.gettext("# Set a nice color palette for plots"))
+  doItAndPrint('lattice.options(default.theme=custom.theme(symbol=brewer.pal(8, "Set1")[c(2:1, 3:5, 7:9)], fill=brewer.pal(8, "Set1")[c(2:1, 3:5, 7:9)]))')
+
+  doItAndPrint("")
+}
+
