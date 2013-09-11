5959af8fdd97f09294c0f0b9b8038db53761fabd
Milan Bouchet-Valat <nalimilan@club.fr>
2013-09-11 16:11:45 +0000

    Factiva: stop calling private tm functions
    New CRAN policies forbid this. Just copy the (short) functions
    to the package as done with other plug-ins.
    
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/r-temis@479 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tm.plugin.factiva/R/FactivaSource.R
+++ pkg/tm.plugin.factiva/R/FactivaSource.R
@@ -18,7 +18,7 @@ FactivaSource <- function(x, encoding = "UTF-8", format = c("auto", "XML", "HTML
         content <- XML::getNodeSet(tree, "//div[starts-with(@class, 'article')]")
         XML::free(tree)
 
-        s <- tm:::.Source(readFactivaHTML, encoding, length(content), FALSE, seq(1, length(content)), 0, FALSE)
+        s <- .Source(readFactivaHTML, encoding, length(content), FALSE, seq(1, length(content)), 0, FALSE)
         s$Content <- content
         s$URI <- match.call()$x
         class(s) = c("FactivaSource", "Source")
@@ -26,7 +26,21 @@ FactivaSource <- function(x, encoding = "UTF-8", format = c("auto", "XML", "HTML
     }
 }
 
+# Taken from tm
+.Source <- function(defaultreader, encoding, length, lodsupport, names, position, vectorized, class = NULL) {
+    if (vectorized && (length <= 0))
+        stop("vectorized sources must have positive length")
+
+    if (!is.null(names) && (length != length(names)))
+        stop("incorrect number of element names")
+
+    structure(list(DefaultReader = defaultreader, Encoding = encoding, Length = length,
+                   LoDSupport = lodsupport, Names = names, Position = position, Vectorized = vectorized),
+              class = unique(c(class, "Source")))
+}
+
 # These functions need to be exactly the same as those for XMLSource
 # since they can be used with the Factiva XML source
-getElem.FactivaSource <- function(x) tm:::getElem.XMLSource(x)
-eoi.FactivaSource <- function(x) tm:::eoi.XMLSource(x)
+# These functions are the same as those for XMLSource
+getElem.FactivaSource <- function(x) list(content = XML::saveXML(x$Content[[x$Position]]), uri = x$URI)
+eoi.FactivaSource <- function(x) length(x$Content) <= x$Position
