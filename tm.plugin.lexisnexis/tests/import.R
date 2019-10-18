88be1c53c7c63df183df48a3ffa68ed66d829a25
Tom Nicholls <github@tomnicholls.me.uk>
2019-10-18 11:14:48 +0100

    Handle LexisNexis giving us an error 'document' instead of an article (#4)
    Also handle absences of LexisNexis copyright statement.

--- tm.plugin.lexisnexis/tests/import.R
+++ tm.plugin.lexisnexis/tests/import.R
@@ -10,3 +10,13 @@ corpus <- Corpus(LexisNexisSource(file))
 file <- system.file("texts", "lexisnexis_test_fr.html",
                     package = "tm.plugin.lexisnexis")
 corpus <- Corpus(LexisNexisSource(file))
+
+# Two malformed examples: one which can be parsed but with a missing copyright
+# notice (and with a warning), and one which should be dropped (with a warning).
+# We suppress warnings to avoid breaking the build, then test the consequences.
+file <- system.file("texts", "lexisnexis_test_copyright_error.html",
+                    package = "tm.plugin.lexisnexis")
+corpus <- suppressWarnings(Corpus(LexisNexisSource(file)))
+stopifnot(length(corpus) == 1,
+          corpus[[1]]$meta$id == "resize201812311",
+          length(corpus[[1]]$meta$rights) == 0)
