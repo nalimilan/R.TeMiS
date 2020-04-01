98154b2bf992c5721f98d6702d994a6dec921470
Milan Bouchet-Valat <nalimilan@club.fr>
2020-04-01 10:05:57 +0200

    SnowballC: Bump Snowball to 2.0.0

---
+++ SnowballC/src/SnowballC_init.c
@@ -0,0 +1,21 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* .Call calls */
+extern SEXP R_getStemLanguages();
+extern SEXP R_stemWords(SEXP, SEXP);
+
+static const R_CallMethodDef CallEntries[] = {
+    {"R_getStemLanguages", (DL_FUNC) &R_getStemLanguages, 0},
+    {"R_stemWords",        (DL_FUNC) &R_stemWords,        2},
+    {NULL, NULL, 0}
+};
+
+void R_init_SnowballC(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
+
