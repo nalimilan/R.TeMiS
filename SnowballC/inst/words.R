fef4c48ae0571555cf1f4d5d4c1c328ef0db446e
Milan Bouchet-Valat <nalimilan@club.fr>
2020-04-01 10:05:57 +0200

    SnowballC: Bump Snowball to 2.0.0

--- SnowballC/inst/words.R
+++ SnowballC/inst/words.R
@@ -1,31 +1,18 @@
 # The following code can be used to read vocabulary lists from
-# the data directory of svn://svn.tartarus.org/snowball/
+# https://github.com/snowballstem/snowball-data
 # Manual fixes are needed to replace empty values by ""
 for(lang in getStemLanguages()) {
-    voc <- scan(file.path("words", lang, "diffs.txt"),
-                list("character", "character"), quote="\"", quiet=TRUE)
+    cat(lang, "\n")
+    vocf <- file.path("snowball-data", lang, "voc.txt")
+    if(!file.exists(vocf)) vocf <- file.path("snowball-data", lang, "voc.txt.gz")
+    outputf <- file.path("snowball-data", lang, "output.txt")
+    if(!file.exists(outputf)) outputf <- file.path("snowball-data", lang, "output.txt.gz")
+    voc <- readLines(vocf, encoding="UTF-8")
+    output <- readLines(outputf, encoding="UTF-8")
+    stopifnot(all(wordStem(voc, lang) == output))
 
-    voc <- data.frame(word=voc[[1]], stem=voc[[2]], stringsAsFactors=FALSE)
-
-    stopifnot(all(wordStem(voc[[1]], lang) == voc[[2]]))
+    dat <- data.frame(word=voc, stem=output, stringsAsFactors=FALSE)
+    # Only keep a subsample of words to reduce space needed for CRAN releases
+    dat <- dat[seq(1, nrow(dat), length.out=1000),]
+    save(dat, file=file.path("words", paste0(lang, ".RData")), compress="xz")
 }
-
-# Save individual files in the most compressed form
-for(lang in getStemLanguages()) {
-    voc <- scan(file.path("words", lang, "diffs.txt"),
-                list("character", "character"), quote="\"", quiet=TRUE)
-
-    voc <- data.frame(word=voc[[1]], stem=voc[[2]], stringsAsFactors=FALSE)
-
-    save(voc, file=file.path("words", paste0(lang, ".RData")), compress="xz")
-}
-
-# Only keep a subsample of words to reduce space needed for CRAN releases
-for(lang in getStemLanguages()) {
-    load(file.path("words", paste0(lang, ".RData")))
-
-    voc <- voc[seq(1, nrow(voc), by=80),]
-
-    save(voc, file=file.path("words", paste0(lang, ".RData")), compress="xz")
-}
-
